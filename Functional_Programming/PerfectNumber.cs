ddf2652cb14b4b757f3e672bc123eacd18220f32
Prasoon-mishra <prasoonmishra987@gmail.com>
2022-08-15 01:32:57 +0530

    [Prasoon] Added Perfect Number

---
+++ Functional_Programming/PerfectNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functional_Programming
+{
+    public class PerfectNumber
+    {
+        public void Perfect()
+        {
+            Console.WriteLine("Enter the Number");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            int i,sum = 0;
+            for(i = 1; i < Number; i++)
+            {
+                if(Number%i==0)
+                {
+                    sum=sum+i;
+                }
+            }
+            if(sum==Number)
+            {
+                Console.WriteLine("Number is Perfect");
+            }
+            else
+            {
+                Console.WriteLine("Number is not Perfect");
+            }
+        }
+    }
+}
--- Functional_Programming/Program.cs
+++ Functional_Programming/Program.cs
@@ -6,11 +6,15 @@
         {
             Console.WriteLine("Hello, Welcome to Functional Programming");
             Console.WriteLine("Enter 1 for Fibonacci Series");
+            Console.WriteLine("Enter 2 for Perfect Number");
             int Ans=Convert.ToInt32(Console.ReadLine());
             switch(Ans)
             {
                 case 1: Fibonacci FibonacciObj = new Fibonacci();
                     FibonacciObj.Fibonacci_Series();
+                    break;
+                case 2: PerfectNumber PerfectObj= new PerfectNumber();
+                    PerfectObj.Perfect();
                     break;
                 default: Console.WriteLine("Entered Value is Wrong");
                     break;
